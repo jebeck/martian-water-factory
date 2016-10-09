733a6a80ca5b3f1c5e356fdbed002b9d1e8b4cab
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-10-09 00:04:34 -0700

    rewrite App as a ✨pure✨ functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -7,27 +7,9 @@ import { burnWoodShavings, toggleHydrazineValve } from './state';
 
 import './App.css';
 
-export class App extends Component {
-  static propTypes = {
-    api: PropTypes.shape({
-      burnWoodShavings: PropTypes.func.isRequired,
-    }),
-    burnWoodShavings: PropTypes.func.isRequired,
-    explosion: PropTypes.bool.isRequired,
-    fireBurning: PropTypes.bool.isRequired,
-    hydrazine: PropTypes.number.isRequired,
-    hydrazineValveOpen: PropTypes.bool.isRequired,
-    hydrogen: PropTypes.number.isRequired,
-    oxygen: PropTypes.shape({
-      tank1: PropTypes.number.isRequired,
-      tank2: PropTypes.number.isRequired,
-    }).isRequired,
-    toggleHydrazineValve: PropTypes.func.isRequired,
-    water: PropTypes.number.isRequired,
-  };
-
-  renderExplosion() {
-    if (this.props.explosion) {
+export const App = (props) => {
+  function renderExplosion() {
+    if (props.explosion) {
       return (
         <h1 className="Explosion"><marquee>BOOM! BOOM! BOOM!</marquee></h1>
       );
@@ -35,38 +17,54 @@ export class App extends Component {
     return null;
   }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="Hab">
-          {this.renderExplosion()}
-          <div className="OxygenTank">
-            <h3>O₂ Tank #1</h3>
-            <p>{`${Math.round(this.props.oxygen.tank1)}L`}</p>
-          </div>
-          <div className="Hydrazine">
-            <h3>Hydrazine</h3>
-            <p>{`${this.props.hydrazine}L`}</p>
-            <button onClick={this.props.toggleHydrazineValve}>
-              {this.props.hydrazineValveOpen ? 'Close Valve' : 'Open Valve'}
-            </button>
-          </div>
-          <div className="Chimney">
-            <button onClick={this.props.burnWoodShavings}>Burn Wood Shavings</button>
-          </div>
-          <div className="WaterTank">
-            <h3>Water</h3>
-            <p>{`${Math.round(this.props.water)}L`}</p>
-          </div>
-          <div className="OxygenTank">
-            <h3>O₂ Tank #2</h3>
-            <p>{`${Math.round(this.props.oxygen.tank2)}L`}</p>
-          </div>
+  return (
+    <div className="App">
+      <div className="Hab">
+        {renderExplosion()}
+        <div className="OxygenTank">
+          <h3>O₂ Tank #1</h3>
+          <p>{`${Math.round(props.oxygen.tank1)}L`}</p>
+        </div>
+        <div className="Hydrazine">
+          <h3>Hydrazine</h3>
+          <p>{`${props.hydrazine}L`}</p>
+          <button onClick={props.toggleHydrazineValve}>
+            {props.hydrazineValveOpen ? 'Close Valve' : 'Open Valve'}
+          </button>
+        </div>
+        <div className="Chimney">
+          <button onClick={props.burnWoodShavings}>Burn Wood Shavings</button>
+        </div>
+        <div className="WaterTank">
+          <h3>Water</h3>
+          <p>{`${Math.round(props.water)}L`}</p>
+        </div>
+        <div className="OxygenTank">
+          <h3>O₂ Tank #2</h3>
+          <p>{`${Math.round(props.oxygen.tank2)}L`}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+App.propTypes = {
+  api: PropTypes.shape({
+    burnWoodShavings: PropTypes.func.isRequired,
+  }),
+  burnWoodShavings: PropTypes.func.isRequired,
+  explosion: PropTypes.bool.isRequired,
+  fireBurning: PropTypes.bool.isRequired,
+  hydrazine: PropTypes.number.isRequired,
+  hydrazineValveOpen: PropTypes.bool.isRequired,
+  hydrogen: PropTypes.number.isRequired,
+  oxygen: PropTypes.shape({
+    tank1: PropTypes.number.isRequired,
+    tank2: PropTypes.number.isRequired,
+  }).isRequired,
+  toggleHydrazineValve: PropTypes.func.isRequired,
+  water: PropTypes.number.isRequired,
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
